350219e17c41c9f2725a3c60cabec82a8634ff36
kawsikan <kawsinote@gmail.com>
2020-11-02 16:35:07 +0530

    Delete doen

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Your Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
